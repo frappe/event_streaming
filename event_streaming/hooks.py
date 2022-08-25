87da3ab2b0e2d1e79db173ae999d13d9b9ec9f4e
phot0n <ritwikpuri5678@gmail.com>
2022-08-25 17:25:15 +0530

    feat(minor): add event update log hooks

--- event_streaming/hooks.py
+++ event_streaming/hooks.py
@@ -102,13 +102,14 @@ app_license = "MIT"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
-# }
+doc_events = {
+    "*": {
+        "after_insert": "event_streaming.event_streaming.doctype.event_update_log.event_update_log.notify_consumers",
+        "on_update": "event_streaming.event_streaming.doctype.event_update_log.event_update_log.notify_consumers",
+        "on_cancel": "event_streaming.event_streaming.doctype.event_update_log.event_update_log.notify_consumers",
+        "on_trash": "event_streaming.event_streaming.doctype.event_update_log.event_update_log.notify_consumers"
+    }
+}
 
 # Scheduled Tasks
 # ---------------
