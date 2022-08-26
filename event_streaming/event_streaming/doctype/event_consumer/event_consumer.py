fda539832a6775f08d771d4899148d418347c2f7
phot0n <ritwikpuri5678@gmail.com>
2022-08-26 13:25:57 +0530

    chore: remove unused event_consumer_document_type_map cache

--- event_streaming/event_streaming/doctype/event_consumer/event_consumer.py
+++ event_streaming/event_streaming/doctype/event_consumer/event_consumer.py
@@ -32,12 +32,9 @@ class EventConsumer(Document):
 		else:
 			frappe.db.set_value(self.doctype, self.name, "incoming_change", 0)
 
-		frappe.cache().delete_value("event_consumer_document_type_map")
-
 	def on_trash(self):
 		for i in frappe.get_all("Event Update Log Consumer", {"consumer": self.name}):
 			frappe.delete_doc("Event Update Log Consumer", i.name)
-		frappe.cache().delete_value("event_consumer_document_type_map")
 
 	def update_consumer_status(self):
 		consumer_site = get_consumer_site(self.callback_url)
